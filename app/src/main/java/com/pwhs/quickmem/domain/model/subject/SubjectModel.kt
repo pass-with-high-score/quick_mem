ec67d4ce21bc188c832147d9eda0ced9b30080a0
nguyenvhph43636 <nguyenphan01074@gmail.com>
2024-09-25 20:03:59 +0700

    feat: cập nhật tài nguyên

--- app/src/main/java/com/pwhs/quickmem/domain/model/subject/SubjectModel.kt
+++ app/src/main/java/com/pwhs/quickmem/domain/model/subject/SubjectModel.kt
@@ -77,7 +77,215 @@ data class SubjectModel(
                 iconRes = R.drawable.ic_earth_sciences,
                 color = Color(0xFF15b39b),
                 description = "Earth Science is the study of the Earth."
+            ),
+            SubjectModel(
+                10,
+                "Economics",
+                iconRes = R.drawable.ic_economics,
+                color = Color(0xFFeaaa08),
+                description = "Economics is the study of the Earth."
+            ),
+            SubjectModel(
+                11,
+                "Engineering and technology",
+                iconRes = R.drawable.ic_engineering_and_technology,
+                color = Color(0xFF2970ff),
+                description = "Engineering and technology is the study of the Earth."
+            ),
+            SubjectModel(
+                12,
+                "Environmental studies and forestry",
+                iconRes = R.drawable.ic_environmental_studies_and_forestry,
+                color = Color(0xFF7a5af8),
+                description = "Environmental studies and forestry is the study of the Earth."
+            ),
+            SubjectModel(
+                13,
+                "Family and consumer science",
+                iconRes = R.drawable.ic_family_and_consumer_science,
+                color = Color(0xFFeaaa09),
+                description = "Family and consumer science is the study of the Earth."
+            ),
+            SubjectModel(
+                14,
+                "Geography",
+                iconRes = R.drawable.ic_geography,
+                color = Color(0xFFeaaa09),
+                description = "Geography is the study of the Earth."
+            ),
+            SubjectModel(
+                15,
+                "History",
+                iconRes = R.drawable.ic_history,
+                color = Color(0xFFf63d68),
+                description = "History is the study of the Earth."
+            ),
+            SubjectModel(
+                16,
+                "Human physical performance and recreation",
+                iconRes = R.drawable.ic_human_physical_performance_and_recreation,
+                color = Color(0xFF7a5af8),
+                description = "Human physical performance and recreation is the study of the Earth."
+            ),
+            SubjectModel(
+                17,
+                "Interdisciplinary studies",
+                iconRes = R.drawable.ic_interdisciplinary_studies,
+                color = Color(0xFF9076f8),
+                description = "Interdisciplinary studies is the study of the Earth."
+            ),
+            SubjectModel(
+                18,
+                "Journalism, media studies and communication",
+                iconRes = R.drawable.ic_journalism__media_studies_and_communication,
+                color = Color(0xFF7a5af8),
+                description = "Journalism, media studies and communication is the study of the Earth."
+            ),
+            SubjectModel(
+                19,
+                "Languages and cultures",
+                iconRes = R.drawable.ic_languages_and_cultures,
+                color = Color(0xFFd444f1),
+                description = "Languages and cultures is the study of the Earth."
+            ),
+            SubjectModel(
+                20,
+                "Law",
+                iconRes = R.drawable.ic_languages_and_cultures,
+                color = Color(0xFF7a5af8),
+                description = "Law is the study of the Earth."
+            ),
+            SubjectModel(
+                21,
+                "Library and museum studies",
+                iconRes = R.drawable.ic_library_and_museum_studies,
+                color = Color(0xFFf63d68),
+                description = "Library and museum studies is the study of the Earth."
+            ),
+            SubjectModel(
+                22,
+                "Literature",
+                iconRes = R.drawable.ic_literature,
+                color = Color(0xFFf63d68),
+                description = "Literature is the study of the Earth."
+            ),
+            SubjectModel(
+                23,
+                "Logic",
+                iconRes = R.drawable.ic_logic,
+                color = Color(0xFF4180fe),
+                description = "Logic is the study of the Earth."
+            ),
+            SubjectModel(
+                24,
+                "Mathematics",
+                iconRes = R.drawable.ic_mathematics,
+                color = Color(0xFF2970ff),
+                description = "Mathematics is the study of the Earth."
+            ),
+            SubjectModel(
+                25,
+                "Medicine",
+                iconRes = R.drawable.ic_medicine,
+                color = Color(0xFF7a5af8),
+                description = "Medicine is the study of the Earth."
+            ),
+            SubjectModel(
+                26,
+                "Military sciences",
+                iconRes = R.drawable.ic_military_sciences,
+                color = Color(0xFF7a5af8),
+                description = "Military sciences is the study of the Earth."
+            ),
+            SubjectModel(
+                27,
+                "Other",
+                iconRes = R.drawable.ic_other,
+                color = Color(0xFF66c61c),
+                description = "Other is the study of the Earth."
+            ),
+            SubjectModel(
+                28,
+                "Philosophy",
+                iconRes = R.drawable.ic_philosophy,
+                color = Color(0xFFf63d68),
+                description = "Philosophy is the study of the Earth."
+            ),
+            SubjectModel(
+                29,
+                "Physics",
+                iconRes = R.drawable.ic_physics,
+                color = Color(0xFF17b89f),
+                description = "Physics is the study of the Earth."
+            ),
+            SubjectModel(
+                30,
+                "Political science",
+                iconRes = R.drawable.ic_political_science,
+                color = Color(0xFFeaaa08),
+                description = "Political science is the study of the Earth."
+            ),
+            SubjectModel(
+                31,
+                "Psychology",
+                iconRes = R.drawable.ic_psychology,
+                color = Color(0xFFeaaa08),
+                description = "Psychology is the study of the Earth."
+            ),
+            SubjectModel(
+                32,
+                "Public administration",
+                iconRes = R.drawable.ic_public_administration,
+                color = Color(0xFFeaaa08),
+                description = "Public administration is the study of the Earth."
+            ),
+            SubjectModel(
+                33,
+                "Religion and divinity",
+                iconRes = R.drawable.ic_religion_and_divinity,
+                color = Color(0xFFf63d68),
+                description = "Religion and divinity is the study of the Earth."
+            ),
+            SubjectModel(
+                34,
+                "Social work",
+                iconRes = R.drawable.ic_social_work,
+                color = Color(0xFFeaaa09),
+                description = "Social work is the study of the Earth."
+            ),
+            SubjectModel(
+                35,
+                "Sociology",
+                iconRes = R.drawable.ic_sociology,
+                color = Color(0xFFeaaa09),
+                description = "Sociology is the study of the Earth."
+            ),
+            SubjectModel(
+                36,
+                "Space sciences",
+                iconRes = R.drawable.ic_space_sciences,
+                color = Color(0xFF1ab8a0),
+                description = "Space sciences is the study of the Earth."
+            ),
+            SubjectModel(
+                37,
+                "Systems science",
+                iconRes = R.drawable.ic_systems_science,
+                color = Color(0xFFf64b73),
+                description = "Systems science is the study of the Earth."
+            ),
+            SubjectModel(
+                38,
+                "Transportation",
+                iconRes = R.drawable.ic_transportation,
+                color = Color(0xFF8d72f8),
+                description = "Transportation is the study of the Earth."
             )
+
+
+
+
+
         )
     }
 }
