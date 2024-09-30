be9c2466f6d0347245cdb61d026b68dc5a058033
Nguyễn Quang Minh (NQM) <94773751+nqmgaming@users.noreply.github.com>
2024-09-30 13:24:36 +0700

    Update LoginUiEvent.kt

--- app/src/main/java/com/pwhs/quickmem/presentation/auth/login/LoginUiEvent.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/auth/login/LoginUiEvent.kt
@@ -5,5 +5,4 @@ sealed class LoginUiEvent {
     data object LoginWithEmail : LoginUiEvent()
     data object LoginWithGoogle : LoginUiEvent()
     data object LoginWithFacebook : LoginUiEvent()
-
 }
