3c31d6a966fafe45486742a041c08ef573411451
thinguyen <inuyasha4002@gmail.com>
2024-10-01 20:19:43 +0700

    tech: Chỉnh lại minSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.pwhs.quickmem"
-        minSdk = 27
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
