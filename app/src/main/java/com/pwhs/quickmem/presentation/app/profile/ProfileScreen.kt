4c655084645c4f494ce737a57b7830fdcfcb1268
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-10-01 17:37:58 +0700

    feat: Làm chức năng đăng xuất ở màn profile :)) Đỡ khỏi xoá dữ liệu ứng dụng hoặc cài lại nhé

--- app/src/main/java/com/pwhs/quickmem/presentation/app/profile/ProfileScreen.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/profile/ProfileScreen.kt
@@ -1,22 +1,70 @@
 package com.pwhs.quickmem.presentation.app.profile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.generated.destinations.WelcomeScreenDestination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Composable
 @Destination<RootGraph>
-fun ProfileScreen(modifier: Modifier = Modifier) {
+fun ProfileScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ProfileViewModel = hiltViewModel(),
+    navigator: DestinationsNavigator
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                is ProfileUiEvent.LoadProfile -> TODO()
+                ProfileUiEvent.Logout -> {
+                    navigator.navigateUp()
+                    navigator.navigate(WelcomeScreenDestination) {
+                        popUpTo(WelcomeScreenDestination) { inclusive = true }
+                    }
+                }
+            }
+
+        }
+    }
+    Profile(
+        onLogout = { viewModel.onEvent(ProfileUiAction.Logout) }
+    )
+}
+
+@Composable
+fun Profile(
+    modifier: Modifier = Modifier,
+    onLogout: () -> Unit = {}
+) {
     Scaffold { innerPadding ->
         Column(
-            modifier = modifier.padding(innerPadding),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Text("Profile Screen")
+            Button(
+                onClick = onLogout
+            ) {
+                Text("Logout")
+            }
         }
     }
 }
---
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/profile/ProfileViewModel.kt
@@ -0,0 +1,51 @@
+package com.pwhs.quickmem.presentation.app.profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pwhs.quickmem.core.datastore.AppManager
+import com.pwhs.quickmem.core.datastore.TokenManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val appManager: AppManager,
+    private val tokenManager: TokenManager
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(ProfileUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _uiEvent = Channel<ProfileUiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    init {
+        onEvent(ProfileUiAction.LoadProfile)
+    }
+
+    fun onEvent(event: ProfileUiAction) {
+        when (event) {
+            ProfileUiAction.LoadProfile -> loadProfile()
+            ProfileUiAction.Logout -> logout()
+        }
+    }
+
+    private fun loadProfile() {
+        viewModelScope.launch {
+            val userId = appManager.userId.firstOrNull() ?: return@launch
+        }
+    }
+
+    private fun logout() {
+        viewModelScope.launch {
+            appManager.clearAllData()
+            tokenManager.clearTokens()
+            _uiEvent.send(ProfileUiEvent.Logout)
+        }
+    }
+}
