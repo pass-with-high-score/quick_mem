b591828367600641bc473359eb95126fd6b5de93
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-10-02 20:33:30 +0700

    feat: setup cơ bản

---
+++ app/src/main/java/com/pwhs/quickmem/core/component/DialogSuccess.kt
@@ -0,0 +1,9 @@
+package com.pwhs.quickmem.core.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DialogSuccess(modifier: Modifier = Modifier) {
+
+}
