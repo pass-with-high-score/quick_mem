13915ad2da88897f21ead18e21d1ff485bcc891b
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-10-02 21:44:42 +0700

    feat: thêm logo bear cho màn splash

--- app/src/main/java/com/pwhs/quickmem/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/splash/SplashScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,10 +23,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.pwhs.quickmem.R
 import com.pwhs.quickmem.ui.theme.Abrilfatface_Font
 import com.pwhs.quickmem.util.splashBackground
 import com.ramcosta.composedestinations.annotation.Destination
@@ -109,15 +115,27 @@ fun AnimatedText(modifier: Modifier = Modifier) {
         enter = fadeIn(animationSpec = tween(2000)),
         exit = fadeOut(animationSpec = tween(2000))
     ) {
-        Text(
-            text = "QUICK MEM",
-            fontSize = 32.sp,
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-            fontFamily = Abrilfatface_Font,
-            textAlign = TextAlign.Center,
-            modifier = modifier
-        )
+        Row(
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_bear),
+                contentDescription = "Quick Mem Logo",
+                modifier = modifier
+                    .size(48.dp)
+                    .padding(end = 8.dp)
+            )
+            Text(
+                text = "QUICK MEM",
+                fontSize = 32.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontFamily = Abrilfatface_Font,
+                textAlign = TextAlign.Center,
+                modifier = modifier
+            )
+        }
     }
 }
 
