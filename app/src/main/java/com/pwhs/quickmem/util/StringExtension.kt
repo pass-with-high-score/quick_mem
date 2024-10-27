8565fdc0ffc1cbc4e100c87b6dfe0a464ecad79d
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-10-27 09:49:02 +0700

    fix: sửa lỗi validate password

--- app/src/main/java/com/pwhs/quickmem/util/StringExtension.kt
+++ app/src/main/java/com/pwhs/quickmem/util/StringExtension.kt
@@ -3,41 +3,10 @@ package com.pwhs.quickmem.util
 import androidx.compose.ui.graphics.Color
 import com.wajahatkarim3.easyvalidation.core.view_ktx.validEmail
 import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
 import java.util.Locale
 
 fun String.upperCaseFirstLetter(): String {
     return this.replaceFirstChar { it.uppercase() }
-}
-
-fun String.toDateFormatted(): String {
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-    val date = dateFormat.parse(this) ?: return this
-    return dateFormat.format(date)
-}
-
-fun String.isDateSmallerThan(date: String): Boolean {
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-
-    // Parse the current string (birth date) and the comparison date
-    val birthDate: Date = dateFormat.parse(this) ?: return false
-    val comparisonDate: Date = dateFormat.parse(date) ?: return false
-
-    // Calculate the difference in years
-    val calendarBirth = Calendar.getInstance().apply { time = birthDate }
-    val calendarCompare = Calendar.getInstance().apply { time = comparisonDate }
-
-    // Calculate the age
-    var age = calendarCompare.get(Calendar.YEAR) - calendarBirth.get(Calendar.YEAR)
-
-    // Adjust age if the current date is before the birthday in the comparison year
-    if (calendarCompare.get(Calendar.DAY_OF_YEAR) < calendarBirth.get(Calendar.DAY_OF_YEAR)) {
-        age--
-    }
-
-    // Check if the age is smaller than 18
-    return age < 20
 }
 
 fun String.getUsernameFromEmail(): String {
@@ -57,7 +26,7 @@ fun String.emailIsValid(): Boolean {
 }
 
 fun String.strongPassword(): Boolean {
-    val regex = "^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$".toRegex()
+    val regex = "^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$".toRegex()
     return this.matches(regex)
 }
 
