7c0c4161abd051234e5e2334e553de37b410c3af
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-10-28 20:30:24 +0700

    feat: pull to refresh cho study set detail

--- app/src/main/java/com/pwhs/quickmem/presentation/app/library/study_set/ListStudySetScreen.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/library/study_set/ListStudySetScreen.kt
@@ -8,11 +8,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,6 +25,7 @@ import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.buildAnnotatedString
@@ -79,7 +80,8 @@ fun ListStudySetScreen(
                             model = userAvatar,
                             contentDescription = "User avatar",
                             modifier = Modifier
-                                .size(60.dp),
+                                .size(60.dp)
+                                .clip(CircleShape),
                             contentScale = ContentScale.Crop
                         )
                         Text(
@@ -185,7 +187,10 @@ fun ListStudySetScreen(
                                             AsyncImage(
                                                 model = studySet.user.avatarUrl,
                                                 contentDescription = "User avatar",
-                                                modifier = Modifier.size(18.dp)
+                                                contentScale = ContentScale.Crop,
+                                                modifier = Modifier
+                                                    .size(18.dp)
+                                                    .clip(CircleShape)
                                             )
                                             Text(
                                                 studySet.user.username,
--- app/src/main/java/com/pwhs/quickmem/presentation/app/study_set/detail/component/StudySetDetailTopBar.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/study_set/detail/component/StudySetDetailTopBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.Icons.Default
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -25,7 +26,9 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -64,8 +67,10 @@ fun StudySetDetailTopAppBar(
                     AsyncImage(
                         model = userResponse.avatarUrl,
                         contentDescription = "User Avatar",
+                        contentScale = ContentScale.Crop,
                         modifier = Modifier
                             .size(24.dp)
+                            .clip(CircleShape)
                             .padding(end = 8.dp)
                     )
                     Text(
