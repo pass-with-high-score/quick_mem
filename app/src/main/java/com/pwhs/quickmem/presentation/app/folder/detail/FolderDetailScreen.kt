5c91af7bc7a478f357258a00490a59f21f03032f
Daocon <134820492+Daocon@users.noreply.github.com>
2024-10-30 02:54:45 +0700

    feat: pull to refresh list flashcard in folder detail

--- app/src/main/java/com/pwhs/quickmem/presentation/app/folder/detail/FolderDetailScreen.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/folder/detail/FolderDetailScreen.kt
@@ -1,10 +1,18 @@
 package com.pwhs.quickmem.presentation.app.folder.detail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
@@ -12,14 +20,21 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.pwhs.quickmem.presentation.app.folder.detail.component.ButtonStudyFolderDetail
+import coil.compose.AsyncImage
 import com.pwhs.quickmem.presentation.app.folder.detail.component.FolderDetailTopAppBar
+import com.pwhs.quickmem.presentation.app.folder.detail.component.FolderMenuBottomSheet
 import com.pwhs.quickmem.presentation.app.folder.detail.component.FolderSortOptionBottomSheet
-import com.pwhs.quickmem.presentation.app.folder.detail.component.FolderSortOptionTextButton
+import com.pwhs.quickmem.presentation.component.QuickMemAlertDialog
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -51,14 +66,24 @@ fun FolderDetailScreen(
 @Composable
 fun FolderDetail(
     modifier: Modifier = Modifier,
+    isLoading: Boolean = false,
+    onFolderRefresh: () -> Unit = {},
+    username: String = "User",
+    userAvatar: String = "",
+    folders: List<Any> = emptyList(),
+    onEditFolder: () -> Unit = {},
     onStudyFolderDetailClicked: () -> Unit = {},
     onNavigateBack: () -> Unit = {},
     onAddStudySet: () -> Unit = {}
 ) {
+    var refreshState = rememberPullToRefreshState()
     var sortOptionBottomSheet by remember { mutableStateOf(false) }
+    var showMoreBottomSheet by remember { mutableStateOf(false) }
     var currentSortOption by remember { mutableStateOf(SortOptionEnum.RECENT) }
     val sheetSortOptionState = rememberModalBottomSheetState()
     val sheetShowMoreState = rememberModalBottomSheetState()
+    var showDeleteConfirmationDialog by remember { mutableStateOf(false) }
+
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -66,25 +91,89 @@ fun FolderDetail(
                 title = "Folder Title",
                 dateLabel = "Date Label",
                 onNavigateBack = onNavigateBack,
-                onMoreClicked = {  },
+                onMoreClicked = { showMoreBottomSheet = true },
                 onAddStudySet = onAddStudySet,
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = modifier.padding(innerPadding)
-                .padding(horizontal = 16.dp)
-                .padding(top = 24.dp)
-        ) {
-            ButtonStudyFolderDetail(
-                title = "Study",
-                onStudyFolderDetailClicked = onStudyFolderDetailClicked
-            )
-            FolderSortOptionTextButton(
+                onStudyFolderDetailClicked = onStudyFolderDetailClicked,
                 currentSortOption = currentSortOption,
                 onSortOptionClicked = { sortOptionBottomSheet = true }
             )
         }
+    ) { innerPadding ->
+        PullToRefreshBox(
+            modifier = Modifier.fillMaxWidth(),
+            state = refreshState,
+            isRefreshing = isLoading,
+            onRefresh = { onFolderRefresh() }
+        ) {
+            when {
+                folders.isEmpty() -> {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(innerPadding)
+                            .padding(top = 40.dp)
+                            .padding(horizontal = 16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        AsyncImage(
+                            model = userAvatar,
+                            contentDescription = "User avatar",
+                            modifier = Modifier
+                                .size(60.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
+                        Text(
+                            text = "Hello, $username",
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 24.sp
+                            )
+                        )
+                        HorizontalDivider(
+                            thickness = 1.dp,
+                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+                        )
+                        Text(
+                            text = "Get started by searching for a study set or creating your own",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.bodyMedium.copy(
+                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
+                            ),
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
+                    }
+                }
+                else -> {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(innerPadding)
+                    ) {
+                        // Folder content
+                        Text(text = "Folder content")
+                    }
+                }
+            }
+        }
+    }
+    if (showDeleteConfirmationDialog) {
+        QuickMemAlertDialog(
+            onDismissRequest = {
+                showDeleteConfirmationDialog = false
+                showMoreBottomSheet = true},
+            onConfirm = {
+                onEditFolder()
+                showDeleteConfirmationDialog = false
+            },
+            title = "Delete Folder",
+            text = "Are you sure you want to delete this folder?",
+            confirmButtonTitle = "Delete",
+            dismissButtonTitle = "Cancel",
+        )
     }
     FolderSortOptionBottomSheet(
         onSortOptionClicked = {
@@ -93,6 +182,18 @@ fun FolderDetail(
         sortOptionBottomSheet = sortOptionBottomSheet,
         sheetSortOptionState = sheetSortOptionState,
         onDismissRequest = { sortOptionBottomSheet = false }
+    )
+    FolderMenuBottomSheet(
+        onEditFolder = onEditFolder,
+        onDeleteFolder = {
+            showDeleteConfirmationDialog = true
+            showMoreBottomSheet = false
+        },
+        onShareFolder = {},
+        onReportFolder = {},
+        showMoreBottomSheet = showMoreBottomSheet,
+        sheetShowMoreState = sheetShowMoreState,
+        onDismissRequest = { showMoreBottomSheet = false }
     )
 }
 
