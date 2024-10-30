4c31972337e48022d2ec0da357037f160a4a40fd
Daocon <134820492+Daocon@users.noreply.github.com>
2024-10-30 22:02:32 +0700

    feat: xong sắp xếp list study set theo ( recent / title )

--- app/src/main/java/com/pwhs/quickmem/presentation/app/folder/detail/component/ListStudySetInnerFolder.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/folder/detail/component/ListStudySetInnerFolder.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
