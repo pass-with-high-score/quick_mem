c8a085573eced767ed9ebaba7e555c4422b6635d
vanhai7868 <160410558+vanhai7868@users.noreply.github.com>
2024-11-01 21:39:58 +0700

    settings

--- app/src/main/java/com/pwhs/quickmem/presentation/app/settings/SettingsScreen.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/settings/SettingsScreen.kt
@@ -2,11 +2,14 @@ package com.pwhs.quickmem.presentation.app.settings
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -21,6 +24,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -30,6 +34,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pwhs.quickmem.R
 import com.pwhs.quickmem.ui.theme.QuickMemTheme
@@ -37,6 +42,12 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.generated.destinations.WelcomeScreenDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Destination<RootGraph>
 @Composable
@@ -80,6 +91,8 @@ fun Setting(
     onNavigationBack: () -> Unit = {},
     onNavigateToWelcome: () -> Unit = {},
 ) {
+    val scrollState = rememberScrollState()
+
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -108,65 +121,176 @@ fun Setting(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(scrollState),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            OutlinedButton(
-                onClick = onNavigateToWelcome,
-                modifier = Modifier
-                    .fillMaxWidth(),
-                shape = MaterialTheme.shapes.large,
-                border = BorderStroke(
-                    width = 0.5.dp,
-                    color = MaterialTheme.colorScheme.onSurface
-                ),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = MaterialTheme.colorScheme.onSurface
+            Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Các phần cài đặt khác
+                SectionHeader(title = "Thông tin cá nhân")
+                SettingItem(
+                    title = "Tên người dùng",
+                    subtitle = "quickmem",
+                    onClick = {
+                        //
+                    }
                 )
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                SettingItem(title = "Email", subtitle = "quickmem@gmail.com")
+                SettingItem(title = "Đổi mật khẩu")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Học ngoại tuyến
+                SectionHeader(title = "Học ngoại tuyến")
+                SwitchItem(
+                    title = "Lưu học phần để học ngoại tuyến",
+                    subtitle = "Các học phần mới học gần đây nhất của bạn sẽ được tự động lưu"
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Tuỳ chọn
+                SectionHeader(title = "Tuỳ chọn")
+                SwitchItem(title = "Thông báo đẩy")
+                SwitchItem(title = "Hiệu ứng âm thanh")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Giao diện
+                SectionHeader(title = "Giao diện")
+                SettingItem(title = "Hình nền", subtitle = "Mặc định")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Giới thiệu
+                SectionHeader(title = "Giới thiệu")
+                SettingItem(title = "Chính sách quyền riêng tư")
+                SettingItem(title = "Điều khoản dịch vụ")
+                SettingItem(title = "Giấy phép mã nguồn mở")
+                SettingItem(title = "Trung tâm Hỗ trợ")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+
+                OutlinedButton(
+                    onClick = onNavigateToWelcome,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 5.dp)
+                        .fillMaxWidth(),
+                    shape = MaterialTheme.shapes.large,
+                    border = BorderStroke(
+                        width = 0.5.dp,
+                        color = MaterialTheme.colorScheme.onSurface
+                    ),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        contentColor = MaterialTheme.colorScheme.onSurface
+                    )
                 ) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 5.dp)
                     ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Outlined.Logout,
+                                contentDescription = "Log out",
+                                modifier = Modifier.size(30.dp)
+                            )
+                            Text(
+                                "Log out", style = MaterialTheme.typography.bodyLarge.copy(
+                                    fontWeight = FontWeight.Bold
+                                )
+                            )
+                        }
                         Icon(
-                            imageVector = Icons.AutoMirrored.Outlined.Logout,
-                            contentDescription = "Log out",
+                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                            contentDescription = "Navigate to Welcome",
                             modifier = Modifier.size(30.dp)
                         )
-                        Text(
-                            "Log out", style = MaterialTheme.typography.bodyLarge.copy(
-                                fontWeight = FontWeight.Bold
-                            )
-                        )
                     }
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-                        contentDescription = "Navigate to Welcome",
-                        modifier = Modifier.size(30.dp)
-                    )
                 }
-            }
 
-            Image(
-                painter = painterResource(id = R.drawable.ic_logo),
-                contentDescription = "Logo",
-                modifier = Modifier
-                    .padding(16.dp)
-                    .size(50.dp)
-            )
-            Text(
-                text = "QuickMem", style = MaterialTheme.typography.titleMedium.copy(
-                    fontWeight = FontWeight.Bold
+                // Logo
+                Image(
+                    painter = painterResource(id = R.drawable.ic_logo),
+                    contentDescription = "Logo",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .size(50.dp)
+                        .align(Alignment.CenterHorizontally)
                 )
-            )
+                Text(
+                    text = "QuickMem", style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    ),modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
         }
+    }
+}
+
+@Composable
+fun SectionHeader(title: String) {
+    Text(
+        text = title,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.SemiBold,
+        modifier = Modifier.padding(vertical = 8.dp)
+    )
+}
+
+@Composable
+fun SettingItem(title: String, subtitle: String? = null, onClick: () -> Unit = {}) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Text(text = title, fontWeight = FontWeight.Medium)
+        subtitle?.let {
+            Text(text = it, fontSize = 12.sp, color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f))
+        }
+    }
+}
+
+@Composable
+fun SwitchItem(title: String, subtitle: String? = null,onClick: () -> Unit = {}) {
+
+    var checkedState by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable(onClick = onClick),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = title, fontWeight = FontWeight.Medium)
+            subtitle?.let {
+                Text(
+                    text = it,
+                    fontSize = 12.sp,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                )
+            }
+        }
+
+        Switch(
+            checked = checkedState,
+            onCheckedChange = { isChecked ->
+                checkedState = isChecked
+            }
+        )
     }
 }
 
