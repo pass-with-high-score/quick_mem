fe307dcbc03eeaf500573fa1552140df7130846e
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-11-07 08:03:45 +0700

    fix(navigation): sửa lỗi ko thể navigation đến màn profile khi logout và login lại

--- app/src/main/java/com/pwhs/quickmem/presentation/BottomNavItem.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/BottomNavItem.kt
@@ -5,39 +5,47 @@ import com.ramcosta.composedestinations.generated.destinations.ExploreScreenDest
 import com.ramcosta.composedestinations.generated.destinations.HomeScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.LibraryScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.ProfileScreenDestination
+import com.ramcosta.composedestinations.spec.BaseRoute
+import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
 
 sealed class BottomNavItem(
     val title: String,
     val icon: Int,
     val route: String,
+    val direction: DirectionDestinationSpec,
 ) {
     data object Home : BottomNavItem(
         title = "Home",
         icon = R.drawable.ic_home,
-        route = HomeScreenDestination.route
+        route = HomeScreenDestination.route,
+        direction = HomeScreenDestination
     )
 
     data object Explore : BottomNavItem(
         title = "Explore",
         icon = R.drawable.ic_explore,
-        route = ExploreScreenDestination.route
+        route = ExploreScreenDestination.route,
+        direction = ExploreScreenDestination
     )
 
     data object Center : BottomNavItem(
         title = "",
         icon = R.drawable.ic_add_circle,
-        route = "fab"
+        route = "fab",
+        direction = HomeScreenDestination
     )
 
     data object Library : BottomNavItem(
         title = "Library",
         icon = R.drawable.ic_library,
-        route = LibraryScreenDestination.route
+        route = LibraryScreenDestination.route,
+        direction = LibraryScreenDestination
     )
 
     data object Profile : BottomNavItem(
         title = "Profile",
         icon = R.drawable.ic_person,
-        route = ProfileScreenDestination.route
+        route = ProfileScreenDestination.route,
+        direction = ProfileScreenDestination
     )
 }
