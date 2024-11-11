8cf11f9a3b28dccf6644de932877bc639989a8f6
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-11-11 23:43:01 +0700

    lang: đa ngôn ngữ cho màn user detail, bottom bar, profile - sửa đa ngôn ngữ ở màn create, edit flashcard

--- app/src/main/java/com/pwhs/quickmem/presentation/BottomNavItem.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/BottomNavItem.kt
@@ -1,49 +1,50 @@
 package com.pwhs.quickmem.presentation
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import com.pwhs.quickmem.R
 import com.ramcosta.composedestinations.generated.destinations.ExploreScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.HomeScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.LibraryScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.ProfileScreenDestination
-import com.ramcosta.composedestinations.spec.BaseRoute
 import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
 
 sealed class BottomNavItem(
-    val title: String,
-    val icon: Int,
+    @StringRes val title: Int,
+    @DrawableRes val icon: Int,
     val route: String,
     val direction: DirectionDestinationSpec,
 ) {
     data object Home : BottomNavItem(
-        title = "Home",
+        title = R.string.txt_home,
         icon = R.drawable.ic_home,
         route = HomeScreenDestination.route,
         direction = HomeScreenDestination
     )
 
     data object Explore : BottomNavItem(
-        title = "Explore",
+        title = R.string.txt_explore,
         icon = R.drawable.ic_explore,
         route = ExploreScreenDestination.route,
         direction = ExploreScreenDestination
     )
 
     data object Center : BottomNavItem(
-        title = "",
+        title = R.string.txt_center,
         icon = R.drawable.ic_add_circle,
         route = "fab",
         direction = HomeScreenDestination
     )
 
     data object Library : BottomNavItem(
-        title = "Library",
+        title = R.string.txt_library,
         icon = R.drawable.ic_library,
         route = LibraryScreenDestination.route,
         direction = LibraryScreenDestination
     )
 
     data object Profile : BottomNavItem(
-        title = "Profile",
+        title = R.string.txt_profile,
         icon = R.drawable.ic_person,
         route = ProfileScreenDestination.route,
         direction = ProfileScreenDestination
