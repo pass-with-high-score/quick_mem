8c32dc6c54f27456a87bbcb8451667d57cd8400a
VuDucHuan2811 <130039564+VuDucHuan2811@users.noreply.github.com>
2024-11-12 15:22:36 +0700

    fix: sửa lỗi hiển thị danh sách thông báo màn hình notification

--- app/src/main/java/com/pwhs/quickmem/presentation/app/notification/NotificationUiAction.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/notification/NotificationUiAction.kt
@@ -1,7 +1,4 @@
 package com.pwhs.quickmem.presentation.app.notification
 
 sealed class NotificationUiAction {
-    data object LoadNotifications : NotificationUiAction()
-    data class MarkAsRead(val notificationId: String) : NotificationUiAction()
-    data class DeleteNotification(val notificationId: String) : NotificationUiAction()
 }
