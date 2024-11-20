be9a8a4764c6c29cd196cd9aea138140a26d2bca
thinguyen <inuyasha4002@gmail.com>
2024-11-20 23:18:57 +0700

    feat(profile):Achievement Section xóa import k sd

--- app/src/main/java/com/pwhs/quickmem/presentation/app/profile/ProfileScreen.kt
+++ app/src/main/java/com/pwhs/quickmem/presentation/app/profile/ProfileScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
